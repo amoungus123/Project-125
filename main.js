9f04a3cc1311d026976b82f04c29b60bd88ad36a
amoungus123 <76024871+amoungus123@users.noreply.github.com>
2021-10-23 21:30:03 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(650,600);
+    video.position(260,150);
+
+    canvas = createCanvas(650,500);
+    canvas.position(960,200);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized!');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + "noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference);
+    }
+}
+
+function draw() {
+    background('#969A97');
+    document.getElementById("square_side").innerHTML = "The font size is " + difference + "px"
+    fill(207, 159, 255);
+    stroke(93, 63, 211);
+    square(noseX, noseY, difference);
+}
